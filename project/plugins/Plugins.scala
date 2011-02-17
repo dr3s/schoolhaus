556bdb77b4cd2feeec932414fe94b520989884c1
dr3s <dr3s@github.com>
2011-02-16 21:55:22 -0800

    prj config

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,1 +1,9 @@
-import sbt._class Plugins(info: ProjectInfo) extends PluginDefinition(info) {  val akkaRepo = "Akka Repo" at "http://akka.io/repository"  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0"    }
+import sbt._
+
+class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val bumRepo = "Bum Networks Release Repository" at "http://repo.bumnetworks.com/releases"
+  val AkkaRepo = "Akka Repository" at "http://akka.io/repository"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1-SNAPSHOT"
+  val sbtAkkaBivy = "net.evilmonkeylabs" % "sbt-akka-bivy" % "0.3.7"
+
+}
