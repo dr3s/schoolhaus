8ee59249762972699435f0e824140d4bde755c58
Andres March <andres.march@gmail.com>
2012-04-13 01:00:21 -0500

    openid

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,60 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models._
+import views._
+import play.api.libs.openid.OpenID
 
-object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+object Application extends Controller with Secured {
+   
+  def index = IsAuthenticated { username => _ =>
+    Ok(
+        html.index(username)
+      )
   }
+  
+  val loginForm = Form(
+    tuple(
+      "email" -> text,
+      "password" -> text
+    ) verifying ("Invalid email or password", result => result match {
+      case (email, password) => true
+        
+    })
+  )
+
+  /**
+   * Login page.
+   */
+  def login = Action { implicit request =>
+    Ok(html.login(loginForm))
+  }
+
+  /**
+   * Handle login form submission.
+   */
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.login(formWithErrors)),
+      user => Async {
+        		OpenID.redirectURL(user._1, "http://www.test.com").map(i => Redirect( i, 302) )
+        }
+    )
+    
+  }
+
+  /**
+   * Logout and clean the session.
+   */
+  def logout = Action {
+    Redirect(routes.Application.login).withNewSession.flashing(
+      "success" -> "You've been logged out"
+    )
+  }
+
+  
+
   
 }
